c5a964aa35f7b93ec9c176573fa6ecb78fa50063
quangpn <quangpn101290@gmail.com>
2021-01-02 09:22:30 +0700

    initial commit

---
+++ apps/intranet-cms/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'intranet-cms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'intranet-cms';
+}
